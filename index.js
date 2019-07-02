51075d8410b8026ea42c8179cb65cd31be341ddc
hemanth kumar <hemanth.kumar@tresmlabs.com>
2019-07-02 19:58:10 +0530

    added config

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 
 
 var redbird = require('redbird')({port: 80});
-
+const redbird = require('redbird')({
+    port: 80,
+    xfwd: false,
+    letsencrypt: {
+      path: __dirname + '/certs',
+      port: 9999
+    },
+    ssl: {
+      http2: true,
+    }
+  });
 
 // LetsEncrypt support
 // With Redbird you can get zero conf and automatic SSL certificates for your domains
