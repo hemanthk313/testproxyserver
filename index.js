20e64392572f0d7285824d60afd8ecbfe51f9e6b
hemanth kumar <hemanth.kumar@tresmlabs.com>
2019-07-02 20:12:06 +0530

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-var redbird = require('redbird')({port: 80});
+// var redbird = require('redbird')({port: 80});
 const redbird = require('redbird')({
     port: 80,
     xfwd: false,
@@ -21,5 +21,5 @@ redbird.register('myjan.ga', 'http://127.0.0.1:3000', {
 //       email: 'hemanthk313@gmail.com', // Domain owner/admin email
 //       production: true, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
 //     }
-  }
+//   }
 });
