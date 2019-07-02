ccd97effa593999862c147c733f226096891db46
hemanth kumar <hemanth.kumar@tresmlabs.com>
2019-07-02 22:52:45 +0530

    added

--- index.js
+++ index.js
@@ -35,10 +35,10 @@ redbird.register('qa.myjan.ga', 'http://127.0.0.1:3000', {
 
 
 redbird.register('engagejet.ml', 'http://127.0.0.1:3000', {
-    ssl: {
-        letsencrypt: {
-            email: 'hemanthk313@gmail.com', // Domain owner/admin email
-            production: false, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
-        }
-    }
+    // ssl: {
+    //     letsencrypt: {
+    //         email: '123123@gmail.com', // Domain owner/admin email
+    //         production: false, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
+    //     }
+    // }
 });
