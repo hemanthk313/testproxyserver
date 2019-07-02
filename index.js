95e193c1e2dcc202a6db562530370ff34d60a3ea
hemanth kumar <hemanth.kumar@tresmlabs.com>
2019-07-02 20:13:25 +0530

    added

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ const redbird = require('redbird')({
 // LetsEncrypt support
 // With Redbird you can get zero conf and automatic SSL certificates for your domains
 redbird.register('myjan.ga', 'http://127.0.0.1:3000', {
-//   ssl: {
-//     letsencrypt: {
-//       email: 'hemanthk313@gmail.com', // Domain owner/admin email
-//       production: true, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
-//     }
-//   }
+  ssl: {
+    letsencrypt: {
+      email: 'hemanthk313@gmail.com', // Domain owner/admin email
+      production: true, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
+    }
+  }
 });
