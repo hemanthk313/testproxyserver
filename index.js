dd9c2f4ac0d45f359da5d61fe56dcd3d4fd196be
hemanth kumar <hemanth.kumar@tresmlabs.com>
2019-07-03 10:14:58 +0530

    added

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
 
 
-// var redbird = require('redbird')({port: 80});
-const redbird = require('redbird')({
-    port: 80,
-    xfwd: true,
-    letsencrypt: {
-        path: __dirname + '/certs',
-        port: 9999
-    },
-    ssl: {
-        http2: true,
-        port: 443, // SSL port used to serve registered https routes with LetsEncrypt certificate.
-    }
-});
+var redbird = require('redbird')({port: 80});
 
-// LetsEncrypt support
-// With Redbird you can get zero conf and automatic SSL certificates for your domains
-redbird.register('myjan.ga', 'http://127.0.0.1:3000', {
-    ssl: {
-        letsencrypt: {
-            email: 'hemanthk313@gmail.com', // Domain owner/admin email
-            production: false, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
-        }
-    }
-});
-redbird.register('qa.myjan.ga', 'http://127.0.0.1:3000', {
-    ssl: {
-        letsencrypt: {
-            email: 'hemanthk313@gmail.com', // Domain owner/admin email
-            production: false, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
-        }
-    }
-});
 
 
 redbird.register('engagejet.ml', 'http://127.0.0.1:3000', {
