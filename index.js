54396bd4b8effc0966494eabbbe46fed936ca99b
hemanth kumar <hemanth.kumar@tresmlabs.com>
2019-07-03 10:45:09 +0530

    add

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var redbird = require('redbird')({
 redbird.register('engagejet.ml', 'http://127.0.0.1:3000', {
     ssl: {
         letsencrypt: {
-            email: '123123@gmail.com', // Domain owner/admin email
+            email: '123@gmail.com', // Domain owner/admin email
             production: true, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
         }
     }
