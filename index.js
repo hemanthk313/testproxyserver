521ffac8d06ce23dd9a7aea08a8a05a45d7cb4d4
hemanth kumar <hemanth.kumar@tresmlabs.com>
2019-07-03 21:17:00 +0530

    add

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-var redbird = require('redbird')({
+const Queue = require('bull');
+
+const redbird = require('redbird')({
     port: 80,
     letsencrypt: {
-      path: __dirname + '/certs',
-      port: 9999
+        path: __dirname + '/certs',
+        port: 9999
     },
     ssl: {
-      port: 443, // <----- this needs to be here
+        port: 443, // <----- this needs to be here
     }
-  });
+});
+
+const Proxy = new Queue('proxy', 'redis://127.0.0.1:6379');
+
+Proxy.process(function (job, done) {
+    console.log(job.data);
+    // job.data contains the custom data passed when the job was created
+    // job.id contains id of this job.
+    // pass it a result
+    const { domain, host, email } = job.data;
+    redbird.register(domain, host, {
+        ssl: {
+            letsencrypt: {
+                email: email, // Domain owner/admin email
+                production: true, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
+            }
+        }
+    });
+    done(null, {});
+
+});
+
+
 redbird.register('engagejet.ml', 'http://127.0.0.1:3000', {
     ssl: {
         letsencrypt: {
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const Queue = require('bull');
+const Proxy = new Queue('proxy', 'redis://127.0.0.1:6379');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello World!'))
-
+app.post('/', (req, res) => {
+    Proxy.add(req.body)
+    res.json(req.body);
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
