f7b660b3d4a1c0687eea5fc302158c61dbed47ae
hemanth kumar <hemanth.kumar@tresmlabs.com>
2019-07-03 11:55:55 +0530

    added

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ redbird.register('engagejet.ml', 'http://127.0.0.1:3000', {
         }
     }
 });
+
+
+redbird.register('myjan.ga', 'http://127.0.0.1:3000', {
+    ssl: {
+        letsencrypt: {
+            email: 'hemanthk313@gmail.com', // Domain owner/admin email
+            production: true, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
+        }
+    }
+});
